5505d43a9ad8a29573fa7175163bbf8be3bd278f
donghwi park <35325234+donghwipark@users.noreply.github.com>
2022-03-02 20:55:25 +0900

    set input output on each insert

--- main.js
+++ main.js
@@ -5,4 +5,20 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-rl.question('Please input your name to start Tic Tac Toe game with Computer!\n', (input) => console.log(input));
+// Start with players input name
+rl.question('Please input your name to start Tic Tac Toe\n', (name) => game(name));
+
+const game = (playerName) => {
+  console.log(`Welcome ${playerName} lets start the game with our Computer!\n`);
+  rl.setPrompt('Input your position \n');
+  rl.prompt();
+
+  rl.on('line', (input) => {
+    if (input === 'end') {
+      console.log('End the game');
+      rl.close();
+    } else {
+      console.log('\nPlease input your position to keep the game\n');
+    }
+  });
+};
